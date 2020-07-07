cca3c1930272e5820d940d1125d16479dd3c731b
linh phan <linhphan@linhs-MacBook-Air.local>
2020-07-06 23:22:27 -0400

    Changes are made 07/06/20

--- src/Models/Server/connection.js
+++ src/Models/Server/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: "127.0.0.1",
 	user: "root",
-	password: "iloveyouIU123!",
+	password: "password",
 	database: "SOS_Challenge"
 });
 
