ad916e7ab3ec02a50dfb634c0e5ad9de868990d7
linh phan <linhphan@linhs-MacBook-Air.local>
2020-07-09 00:08:45 -0400

    Latest

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./node_modules/webpack-dev-server/bin/webpack-dev-server.js
+worker: ./node_modules/webpack-dev-server/bin/webpack-dev-server.js
--- src/Models/Server/connection.js
+++ src/Models/Server/connection.js
@@ -4,7 +4,8 @@ var connection = mysql.createConnection({
 	host: "dno6xji1n8fm828n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 	user: "txpzh1mifchp5uxq",
 	password: "zgapwnjg44tygeph",
-	database: "xymi20vxow5buos8"
+	database: "xymi20vxow5buos8",
+	port: '3306'
 });
 
 connection.connect((err)=> {
