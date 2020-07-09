93187dacba5f2b8c7fb3a1c80f8a864c7e081967
linh phan <linhphan@linhs-MacBook-Air.local>
2020-07-08 22:05:21 -0400

    Added webpack.prod

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,9 @@ module.exports = {
 		filename: 'js/bundle.js'
 	}, 
 	devServer: {
-		contentBase: './dist'
+		contentBase: './dist',
+		compress: true,
+		port: process.env.PORT || 80
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
