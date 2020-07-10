df97939070d027be79f2c2efb3777ea4b4a6d6c5
linh phan <linhphan@linhs-MacBook-Air.local>
2020-07-10 15:15:28 -0400

    Latest

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ASSET_PATH = process.env.ASSET_PATH || '/';
 const PORT = process.env.PORT || 8080;
 const server_host = process.env.HOST || '0.0.0.0';
 const webpack = require('webpack')
@@ -9,12 +8,10 @@ module.exports = {
 	entry: ['babel-polyfill','./src/js/index.js'], // index.js 
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		publicPath: ASSET_PATH,
 		filename: 'js/bundle.js'
 	}, 
 	devServer: {
 		contentBase: './dist',
-		hot: true,
 		host: server_host,
 		port: PORT,
 		disableHostCheck: true
@@ -23,10 +20,7 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			filename: 'index.html',
 			template: './src/index.html'
-		}),
-		new webpack.DefinePlugin({
-	    	'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
-	    })
+		})
 	], 
 	module: {
 		rules: [
